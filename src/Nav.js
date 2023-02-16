2ab731cbd22607c56c239a7ded14a269be9eccd2
JasonPC <jasonwong287962@gmail.com>
2023-02-16 01:05:43 -0600

    added checkout

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,29 @@
 import { Link } from "react-router-dom"
 import './css/nav.css'
-
+import {useLocation} from "react-router-dom";
+import React, {useState,useEffect} from "react"
 
 export const Nav = () => {
+  let { state } = useLocation();
+    const [allProducts,setAllProducts] = useState([]);
+
+    useEffect(()=>{
+      if( state && allProducts !== state.allProducts ){
+        setAllProducts(state.allProducts);
+      }
+    // console.log(data)
+  
+    })
 
   return (
     <nav>
         <div>WebsiteName</div>
       <div>
-        <Link to="/">Home</Link>
-        <Link to="./Shop">Shop</Link>
+        <Link to="/"  state={{ allProducts: allProducts}}>Home</Link>
+        <Link to="./Shop"  state={{ allProducts: allProducts}}>Shop</Link>
+
+        <Link to="/shop/Checkout"  state={{ allProducts: allProducts}}>Checkout</Link>
+
       </div>
 
     </nav>
