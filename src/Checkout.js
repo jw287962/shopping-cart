e9960585b7b17bb4553be244d49f39c22b392b1b
JasonPC <jasonwong287962@gmail.com>
2023-02-16 01:05:36 -0600

    minor changes to map return on item calculations

--- src/Checkout.js
+++ src/Checkout.js
@@ -10,7 +10,6 @@ const Checkout = (props) => {
   const [allProducts,setProducts]  = useState([]);
 
   useEffect(()=>{
-    console.log(state);
     if(allProducts !== state.allProducts){
       setProducts(state.allProducts);
     }
@@ -18,20 +17,21 @@ const Checkout = (props) => {
 
   })
   return (
-    <div className="body">
+    <div className="body"> <h2>CHECKOUT</h2>
         {allProducts.map( ele=> {
-          console.log(ele);
           if(ele == undefined) return (   <div> TOTAL : {  allProducts.reduce((prev,curr) => prev + (curr.price*curr.count),0)} </div>)
           if(ele.count == 0) return;
           return(
             <div className="cartCheckout" key={ele.name}> 
-                <h3>{ele.name}</h3> <div>{ele.count}x</div><div> Price:${ele.count * ele.price}</div> 
+                <h3>{ele.name}</h3> <div className="checkoutCount"><button>-</button>  {ele.count} <button>+</button></div><div> ${ele.price}</div> 
+                <img src={ele.picture} className="checkoutImg"></img>
+                <div className="itemTotals"> Item Total: ${(ele.count * ele.price).toLocaleString() } </div>
             </div>
           )
         })
         }
-        <h2> TOTAL : ${  allProducts.reduce((prev,curr) => prev + (curr.price*curr.count),0)} </h2>
-        <button>PAY</button>
+          <h2> TOTAL : ${  allProducts.reduce((prev,curr) => prev + (curr.price*curr.count),0).toLocaleString()} </h2>
+          <button className="pay">PAY</button>
     </div>
   )
 }
