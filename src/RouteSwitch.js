8cd2e43f776113111d141be85999b4dac48a5cdc
JasonPC <jasonwong287962@gmail.com>
2023-02-15 19:31:40 -0600

    made a shop componenet

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App'
+import { Shop } from './Shop';
 
 const RouteSwitch = () =>{
 
@@ -8,7 +9,7 @@ const RouteSwitch = () =>{
     <BrowserRouter>
         <Routes>
           <Route path='/' element={<App ></App>}></Route>
-          <Route ></Route>
+          <Route path='/shop' element={<Shop></Shop>} ></Route>
         </Routes>
     </BrowserRouter>
     
---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+
+
+export const Shop = () => {
+  return (
+    <div>
+       <h1>Shopping Cart</h1>
+    
+    </div>
+    
+  )
+
+}
