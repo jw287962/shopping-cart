41ba25a45254fa15334676b8a24f4070d14168b5
JasonPC <jasonwong287962@gmail.com>
2023-02-16 01:04:57 -0600

    nothing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './css/App.css'
-import React from "react";
+import React, {useState,useEffect} from "react";
+import { useLocation } from 'react-router-dom';
 
 const App = () =>{
 
+  // let { state } = useLocation();
+  // const [allProducts,setProducts]  = useState([]);
 
+  // useEffect(()=>{
+  //   console.log(state);
+  //   if(allProducts !== state.allProducts){
+  //     setProducts(state.allProducts);
+  //   }
+  // console.log(data)
+
+  // })
   return (
     <div className='body'>
         <h1>HOME Page</h1>
